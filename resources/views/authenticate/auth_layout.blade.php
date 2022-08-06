5ec1249a2d5806719490d727d3aac1ce540b6c06
giftcup <salometambe@gmail.com>
2022-08-06 23:57:46 +0100

    Implement responsiveness across all forms

--- resources/views/authenticate/auth_layout.blade.php
+++ resources/views/authenticate/auth_layout.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.main')
 
 @section('main')
-<main class="bg-gradient-to-r h-screen flex justify-center items-center">
-    <section class="bg-black w-4/6 h-5/6 flex lg:flex-row items-center justify-center rounded-lg">
+<section class="bg-dark-green h-full w-full lg:grid lg:grid-cols-2">
+    <section class="lg:shrink-0">
         @yield('image')
-        <section class="bg-dark-green text-white basis-3/5 h-full p-5 rounded-r-lg">
-            <header class="text-5xl font-extrabold pt-5">
-                @yield('header')
-            </header>
-            <section>
-                @yield('form')
-            </section>
-            <section>
-                @yield('text')
-            </section>
+    </section>
+    <section class="bg-dark-green text-white p-5">
+        <header class="text-5xl font-extrabold pt-5">
+            @yield('header')
+        </header>
+        <section class="py-4">
+            @yield('form')
+            @yield('text')
         </section>
     </section>
-</main>
+</section>
 @endsection
--- resources/views/authenticate/login.blade.php
+++ resources/views/authenticate/login.blade.php
@@ -1,9 +1,7 @@
-{{-- @extends('authenticate.auth_layout')
+@extends('authenticate.auth_layout')
 
 @section('image')
-    <section class="basis-2/5 h-full bg-threads bg-cover bg-[center_left_-20rem] bg-no-repeat rounded-l-lg">
-
-    </section>
+    <img class="h-48 w-full object-cover lg:h-full" src="{{ asset('images/threads.jpg') }}" alt="">
 @endsection
 
 @section('header')
@@ -25,7 +23,7 @@
             <label class="font-bold text-sm" for="password">Password</label>
             <input type="password" name="password"
                 class="bg-dark-green border-b border-dark-green border-b-orange-red hover:outline-none focus:outline-none py-2 text-lg">
-            @if($errors->has('password'))
+            @if ($errors->has('password'))
                 <div class="validationError">{{ $errors->first('password') }}</div>
             @endif
         </div>
@@ -38,42 +36,4 @@
 @section('text')
     <p>Don't have an account? <a href="{{ route('reg.create') }}"
             class="font-bold underline hover:text-orange-red text-lg">Signup</a></p>
-@endsection --}}
-
-@extends('layouts.main')
-{{-- <section class=""> --}}
-    <section class="bg-dark-green h-full w-full lg:grid lg:grid-cols-2">
-        <section class="lg:shrink-0">
-            <img class="h-48 w-full object-cover lg:h-full" src="{{ asset('images/threads.jpg') }}" alt="">
-        </section>
-        <section class="bg-dark-green text-white p-5">
-            <header class="text-5xl font-extrabold pt-5">
-                <h1>Login</h1>
-            </header>
-            <section class="py-4">
-                <form action="">
-                    <div class="flex flex-col pt-7">
-                        <label class="font-bold text-sm" for="phone_number">Phone number</label>
-                        <input type="text" name="phone_number" value="{{ old('phone_number') }}"
-                            class="bg-dark-green border-b border-dark-green border-b-orange-red hover:outline-none focus:outline-none py-2 text-lg @error('phone_number') is-invalid @enderror">
-                        @error('phone_number')
-                            <div class="validationError">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="flex flex-col pt-7">
-                        <label class="font-bold text-sm" for="password">Password</label>
-                        <input type="password" name="password"
-                            class="bg-dark-green border-b border-dark-green border-b-orange-red hover:outline-none focus:outline-none py-2 text-lg">
-                        @if($errors->has('password'))
-                            <div class="validationError">{{ $errors->first('password') }}</div>
-                        @endif
-                    </div>
-                    <div>
-                        <button type="submit" class="my-9 p-2 bg-orange-red w-full rounded font-bold text-2xl">Login</button>
-                    </div>
-                </form>
-                <p>Don't have an account? <a href="{{ route('reg.create') }}">Signup</a></p>
-            </section>
-        </section>
-    </section>
-{{-- </section> --}}
+@endsection
